504293c4943e4b472288a4cddc2a3382e8f88a7c
Deepak Sharma <93813847+Deepak12347@users.noreply.github.com>
2023-05-29 16:18:35 +0530

    Add files via upload

---
+++ pixhawk_arduino_setup.ino
@@ -0,0 +1,20 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(10, 11); // RX, TX for communication with Pixhawk
+
+void setup() {
+  Serial.begin(57600); // Initialize the serial communication for debugging purposes
+  mySerial.begin(57600); // Initialize the serial communication for communication with Pixhawk
+
+  delay(2000); // Wait for the connection to stabilize
+
+  if (mySerial.available()) {
+    Serial.println("Connection established with Pixhawk");
+  } else {
+    Serial.println("Failed to establish connection with Pixhawk");
+  }
+}
+
+void loop() {
+ 
+}
